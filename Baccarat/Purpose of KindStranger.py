a8e61f1bf5a6508e3adcc6edc41478f52336c349
Dallas Foster <62765251+Dallas-Foster@users.noreply.github.com>
2024-12-13 22:36:10 -0500

    Purpose of KindStranger.py

---
+++ Baccarat/Purpose of KindStranger.py
@@ -0,0 +1,8 @@
+The purpose of the KindStranger program was initially to simulate a basic betting strategy where it uses a machine learning model to predict
+outcomes and place fixed bets accordingly. The agent started with a predefined balance and updates it based on whether its bets win 
+or lose against the actual game outcomes. 
+
+However, this program can be considered obsolete and does not need to be used/downloaded. BaccaratMain.py uses the same MLM but 
+executes it in the main istself. Being the main, it also handles user interaction for placing bets, simulating game outcomes, 
+displaying game history, card details, and managing the overall program.
+
